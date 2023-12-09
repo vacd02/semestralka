cf53479f119a9213825cb4ae45812057c2ccc52c
vacd02 <dawe3.vaclavik@gmail.com>
2023-12-09 12:14:43 +0100

    fixfix

--- src/utils/test/mocks/index.ts
+++
@@ -1,5 +0,0 @@
-import mockProducts from './products';
-import mockCartProducts from './cart-products';
-import mockCartTotal from './cart-total';
-
-export { mockProducts, mockCartProducts, mockCartTotal };
--- src/utils/test/test-utils.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react';
-import { render } from '@testing-library/react';
-
-import { ThemeProvider } from 'commons/style/styled-components';
-import { theme } from 'commons/style/theme';
-
-export const renderWithThemeProvider = (children: ReactNode) => {
-  return render(<ThemeProvider theme={theme}>{children}</ThemeProvider>);
-};
